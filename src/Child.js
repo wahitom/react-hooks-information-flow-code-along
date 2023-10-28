9bca77c5bf9f3fe0c9976799c09eb12c67dca8b0
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-10-28 23:04:57 +0300

    Completed

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor); //this function is the same os handleChangeColor()
+    //which means that newColor is the value of newChildColor and is passed here to cinvoke
+    //getrandomColor() and change the color of parent when child is clicked
+  }
+  console.log(onChangeColor);
+  return (
+    <div
+      onClick={handleClick}
+      className="child"
+      style={{ backgroundColor: color }}
+    />
+  );
 }
 
 export default Child;
